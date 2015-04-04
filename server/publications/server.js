6cf4b840f08eedef8b4b23c0f49bc25b83337569
jrudio <justinjrudio@gmail.com>
2015-04-03 17:26:32 -0700

    Added child publication to return orders of the current user

--- server/publications/server.js
+++ server/publications/server.js
@@ -2,6 +2,12 @@ Meteor.publishComposite('servers', function(){
   return {
     find: function(){
       return Servers.find({available: true}, { sort: { createdAt: -1 }});
-    }
+    },
+    children: [{
+      find: function(){
+        // Return everything for now to determine what I need to return
+        return PlexCustomers.find({ $and: [{ userId: this.userId }, {paymentSuccess: true}] }, { fields: { serverId: 1, userId: 1, paymentSuccess: 1 }});
+      }
+    }]
   };
 });
