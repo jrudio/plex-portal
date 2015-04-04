fc2035ddea189de14fcec9149aa268f92b6d8d84
jrudio <justinjrudio@gmail.com>
2015-04-03 18:33:55 -0700

    Stopped displaying ids to current users' purchased servers

--- client/templates/servers/serverInfo.js
+++ client/templates/servers/serverInfo.js
@@ -33,7 +33,6 @@ Template.server_info.events({
 Template.server_info.helpers({
   purchased: function(_id){
     // Return a boolean
-    console.log(_id);
     var currentUser = Meteor.userId();
     var currentServer = PlexCustomers.find({ $and: [{ userId: currentUser }, {serverId: _id}, { paymentSuccess: true }] }, { fields: { _id: 1 }});
     return currentServer.count() ? true : false;
