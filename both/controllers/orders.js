7fecc6b7ab303f8261e99196b2f691e99cbe744a
jrudio <justinjrudio@gmail.com>
2015-04-04 21:09:38 -0700

    Hide orders from navbar when not logged in

--- both/controllers/orders.js
+++ both/controllers/orders.js
@@ -3,7 +3,10 @@ OrdersController = AppController.extend({
     return this.subscribe('plexCustomer');
   },
   data: {
-    orders: PlexCustomers.find({})
+    orders: PlexCustomers.find({}),
+    currentUsername: function(){
+      return Meteor.user().username;
+    }
   },
   onAfterAction: function () {
     Meta.setTitle('Dashboard');
